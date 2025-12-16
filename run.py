cb3973eea811ae7795e32e0e06c2a28fffe95963
WHS-DTECH <166963235+WHS-DTECH@users.noreply.github.com>
2025-12-16 16:56:25 +1300

    Describe your changes here

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
+
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '../food_inventory'))
 from flask import Flask, render_template
+from app import app as food_inventory_app
 
 app = Flask(__name__, template_folder="maintemplates")
 
@@ -9,6 +14,12 @@ app = Flask(__name__, template_folder="maintemplates")
 @app.route('/')
 def welcome():
     return render_template('welcome.html')
+
+# Route to Food Inventory System at /recbk
+@app.route('/recbk')
+def recbk():
+    # Call the index view of the food_inventory app
+    return food_inventory_app.view_functions['index']()
 
 if __name__ == '__main__':
     app.run(debug=True)
